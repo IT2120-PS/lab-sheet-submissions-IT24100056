3aabf59492b073ac6e671323c0ef242c2114b517
IT24100056 <it24100056@MY.sliit.lk>
2025-09-16 19:32:19 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100056.R
@@ -0,0 +1,17 @@
+getwd()
+setwd("C:\\Users\\HP\\Desktop\\IT24100056_7")
+
+
+# Ques1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+# Ques2
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+# Ques3
+# (i)
+1 - pnorm(130, mean = 100, sd = 15)
+
+# (ii)
+qnorm(0.95, mean = 100, sd = 15)   # 95th percentile
+
